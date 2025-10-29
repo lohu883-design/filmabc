fc6fb0a085a645c309266efbf7d5aa9df13d4c20
lohu883-design <lohu883@gmail.com>
2025-10-29 11:00:41 +0700

    Add files via upload

---
+++ public/tunnel.js
@@ -0,0 +1,41 @@
+function _0x2a1a(t, e) {
+  const r = _0x5cdd();
+  return (_0x2a1a = function(t, e) {
+    return r[t -= 173]
+  })(t, e)
+}
+
+function ddhls012(t) {
+  const e = _0x2a1a,
+    r = {
+      TDjtH: e(185) + e(190) + e(188) + "lJ",
+      nyFak: function(t, e) {
+        return t + e
+      },
+      GhOOo: e(196) + e(192) + e(189)
+    };
+  let i;
+  try {
+    let n = CryptoJS[e(194)][e(187)][e(181)](t)[e(173)](CryptoJS[e(194)][e(195)]);
+    i = CryptoJS[e(177)][e(183)](n, r[e(175)]), i = i[e(173)](CryptoJS[e(194)][e(193)])
+  } catch (t) {
+    console[e(180)](r[e(199)](r[e(174)], t))
+  }
+  return i
+}
+
+function _0x5cdd() {
+  const t = ["enc", "Base64", "Error decr", "1943175lSZoUn", "3884YUQDGw", "nyFak", "toString", "GhOOo", "TDjtH", "44200ZZgPWw", "AES", "2832QeoHuG", "1029fKiseO", "log", "parse", "19010997uUOMIz", "decrypt", "35892uOMcqD", "sHqvxUbI3H", "2062192rRabcH", "Hex", "thFw5e8DCg", "a: ", "RHqHNdluQ5", "1008398FLvNzP", "ypting dat", "Utf8"];
+  return (_0x5cdd = function() {
+    return t
+  })()
+}! function(t, e) {
+  const r = _0x2a1a,
+    i = _0x5cdd();
+  for (;;) try {
+    if (968655 === parseInt(r(176)) / 1 + -parseInt(r(191)) / 2 + -parseInt(r(178)) / 3 * (parseInt(r(198)) / 4) + -parseInt(r(197)) / 5 + -parseInt(r(184)) / 6 * (-parseInt(r(179)) / 7) + -parseInt(r(186)) / 8 + parseInt(r(182)) / 9) break;
+    i.push(i.shift())
+  } catch (t) {
+    i.push(i.shift())
+  }
+}()
